2930b741b3675759b3a8e0291a55542974ae317f
Vladys K <vldk0022@gmail.com>
2024-12-02 21:31:58 +0100

    feat: subscribe logic and cloudinary key done, working on uploading the pictures

---
+++ controllers/users.controller.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const User = require('../models/User.model')
+
+module.exports.create = (req, res, next) => {
+    res.render('users/subscribe')
+  }
+
+  module.exports.doCreate = (req, res, next) => {
+
+    const fields = {
+        ...req.body,
+        image: req.file.path
+      }
+
+    User.create(fields)
+      .then(() => {
+        res.redirect('/')
+      })
+    
+      .catch(error => {
+        // Para autorellenar el formulario cuando haya errores, pasamos todos los valores del req.body, menos la password
+        const values = {...req.body}
+        delete values.password
+  
+        if (error instanceof mongoose.Error.ValidationError) {
+          res.render('users/subscribe', {
+            errors: error.errors,
+            values
+          })
+        } else if (error.code && error.code === 11000) {
+          const errors = {}
+  
+          if (error.keyValue.email) {
+            errors.email = 'Ya existe un usuario con este email'
+          }
+  
+          if (error.keyValue.username) {
+            errors.username = 'Ya existe un usuario con este nombre'
+          }
+  
+          res.render('users/subscribe', { errors, values })
+        } else {
+          next(error)
+        }
+  
+      })
+  }
+
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,12 @@
 const router = require ('express').Router()
 
+const upload = require('../config/multer.config')
+
+const { isAuthenticated, isNotAuthenticated } = require('../middlewares/auth.middleware')
+
+const usersController = require('../controllers/users.controller')
+const authController = require('../controllers/auth.controller')
+
 //Home 
 router.get('/', (req, res, next) => {
     res.render('home');
@@ -17,18 +24,18 @@ router.get('/', (req, res, next) => {
 
 //Users subscribe 
 router.get('/subscribe', isNotAuthenticated, usersController.create)
-router.post('/subscribe', isNotAuthenticated, upload.single('image'), usersController.doCreate)
+router.post('/subscribe', isNotAuthenticated, usersController.doCreate)
 
 //Users sign in 
-router.get('/signin', isNotAuthenticated, authController.login)
-router.post('/signin', isNotAuthenticated, authController.doLogin)
+// router.get('/signin', isNotAuthenticated, authController.login)
+// router.post('/signin', isNotAuthenticated, authController.doLogin)
 
-//Users log out
-router.get('/logout', isAuthenticated, authController.logout)
+// //Users log out
+// router.get('/logout', isAuthenticated, authController.logout)
 
-//Users 
-router.get('/profile', isAuthenticated, usersController.getCurrentUserProfile)
-router.get('/users/:id', isAuthenticated, usersController.getUserProfile)
+// //Users 
+// router.get('/profile', isAuthenticated, usersController.getCurrentUserProfile)
+// router.get('/users/:id', isAuthenticated, usersController.getUserProfile)
 
 
 
